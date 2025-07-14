e6beaa1ffd6328cc0c4a6eb0d873151c35d95da0
vee-mass <vmassela@gmail.com>
2025-07-14 13:13:19 +0200

    fractions

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Runtime.Versioning;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
-    }
+
+        /* Fraction myFraction = new Fraction();
+         myFraction.Settop(3);
+         myFraction.Setbottom(4);
+
+         int topValue = myFraction.Gettop();
+         int bottomValue = myFraction.Getbottom();
+
+         Console.WriteLine($"Top: {topValue} ");
+         Console.WriteLine($"Bottom: {bottomValue}"); */
+
+        Fraction f1 = new Fraction(1);
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction(3, 4);
+        Fraction f4 = new Fraction(1, 5);
+
+
+        Console.WriteLine(f1.GetFractionString()); 
+        Console.WriteLine(f1.GetDecimalValue());     
+        Console.WriteLine(f2.GetFractionString());   
+        Console.WriteLine(f2.GetDecimalValue());   
+        Console.WriteLine(f3.GetFractionString());   
+        Console.WriteLine(f3.GetDecimalValue());     
+        Console.WriteLine(f4.GetFractionString());   
+        Console.WriteLine(f4.GetDecimalValue());     
+
+
+    }  
 }
